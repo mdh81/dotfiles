ef34669d3eb2bc219d778d1b18707347f1a1c5f0
Murali Dhanakoti <dhanakoti.murali@gmail.com>
2026-07-21 20:37:01 -0700

    setup lsp

--- nvim/lua/configs/lspconfig.lua
+++ nvim/lua/configs/lspconfig.lua
@@ -1,6 +1,7 @@
 require("nvchad.configs.lspconfig").defaults()
 
-local servers = { "html", "cssls" }
+local servers = { "basedpyright", "clangd", "cssls", "html" }
 vim.lsp.enable(servers)
 
--- read :h vim.lsp.config for changing options of lsp servers 
+-- Add server-specific overrides with vim.lsp.config(); nvim-lspconfig
+-- provides the defaults for the servers enabled above.
