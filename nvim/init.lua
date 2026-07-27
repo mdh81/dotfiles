9f0fddff354ead1786995184daaba04e715420c7
Murali Dhanakoti <murali.dhanakoti@oracle.com>
2026-07-27 15:33:55 -0700

    global settings and emscripten awareness for clangd

--- nvim/init.lua
+++ nvim/init.lua
@@ -35,3 +35,9 @@ require "autocmds"
 vim.schedule(function()
   require "mappings"
 end)
+
+-- Legacy vim usuals
+
+vim.opt.shiftwidth=4
+vim.opt.tabstop=4
+vim.opt.expandtab=true
--- nvim/lua/configs/lspconfig.lua
+++ nvim/lua/configs/lspconfig.lua
@@ -1,5 +1,17 @@
 require("nvchad.configs.lspconfig").defaults()
 
+local clangd_cmd = { "clangd" }
+if vim.env.EMSDK and vim.env.EMSDK ~= "" then
+  table.insert(
+    clangd_cmd,
+    "--query-driver=" .. vim.fs.joinpath(vim.env.EMSDK, "upstream", "emscripten", "em++")
+  )
+end
+
+vim.lsp.config("clangd", {
+  cmd = clangd_cmd,
+})
+
 local servers = { "basedpyright", "clangd", "cssls", "html" }
 vim.lsp.enable(servers)
 
